e3aaffe186e337d5b8154032d11261945cf0ffd2
Sarah Kim <kimsuyon2013@gmail.com>
2023-06-16 15:08:22 -0700

    lifted state and attempted in creating state for deleting task

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
 
@@ -16,13 +16,33 @@ const TASKS = [
 ];
 
 const App = () => {
+  const [tasks, setTasks] = useState(TASKS);
+
+  const changeCompleteStatus = (id, isComplete) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === id){
+        const updatedTask = {...task}; 
+        updatedTask.isComplete = !isComplete
+      return updatedTask;
+    } else{
+      return {...task};
+    }   
+    })
+    setTasks(newTasks);
+  };
+  const deleteTask = (id) => {
+    const newTasks = tasks.filter((task) => {
+      task.id !== id;
+    })
+    setTasks(newTasks);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>{<TaskList tasks={tasks} updateIsComplete={changeCompleteStatus} delete={deleteTask}/>}</div>
       </main>
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -4,18 +4,19 @@ import PropTypes from 'prop-types';
 import './Task.css';
 
 const Task = (props) => {
-  const [complete, setComplete] = useState(props.isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
-
+  // const [complete, setComplete] = useState(props.isComplete);
+  const buttonClass = props.isComplete ? 'tasks__item__toggle--completed' : '';
+  
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        onClick={() => {props.updateIsComplete(props.id, props.isComplete);}}
       >
         {props.title}
       </button>
-      <button className="tasks__item__remove button">x</button>
+      <button className="tasks__item__remove button" 
+      onClick={() => props.delete(props.id)}>x</button>
     </li>
   );
 };
@@ -24,6 +25,8 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  updateIsComplete: PropTypes.func.isRequired,
+  delete: PropTypes.func.isRequired
 };
 
 export default Task;
